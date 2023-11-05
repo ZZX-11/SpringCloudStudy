5e236c450f2506234242895185505478b1931167
zzx <1102706742@QQ.com>
2023-11-05 20:12:12 +0800

    3.4 passenger 前端页面的开发

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,4 @@
 <template>
-<!--  <nav>-->
-<!--    <router-link to="/">Home</router-link> |-->
-<!--    <router-link to="/about">About</router-link>-->
-<!--  </nav>-->
-<!--  这里可以放一些公共都有的东西-->
-<!--  app页面用于和Index下的app标签关联起来-->
   <router-view/>
 </template>
 
--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,5 +1,38 @@
 <template>
-  <a-button type="primary" @click="onAdd">新增</a-button>
+
+  <a-space>
+    <a-button type="primary" @click="handleQuery()">刷新</a-button>
+    <a-button type="primary" @click="onAdd">新增</a-button>
+  </a-space>
+
+  <a-table :dataSource="passengers"
+           :columns="columns"
+           :pagination="pagination"
+           @change="handleTableChange"
+           :loading="loading">
+    <template #bodyCell="{ column, record }">
+      <template v-if="column.dataIndex === 'operation'">
+        <a-space>
+          <a-popconfirm
+              title="删除后不可恢复，确认删除?"
+              @confirm="onDelete(record)"
+              ok-text="确认" cancel-text="取消">
+            <a style="color: red">删除</a>
+          </a-popconfirm>
+          <a @click="onEdit(record)">编辑</a>
+        </a-space>
+      </template>
+
+      <template v-else-if="column.dataIndex === 'type'">
+        <span v-for="item in PASSENGER_TYPE_ARRAY" :key="item.code">
+          <span v-if="item.code === record.type">
+            {{item.desc}}
+          </span>
+        </span>
+      </template>
+    </template>
+  </a-table>
+
   <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk"
            ok-text="确认" cancel-text="取消">
     <a-form :model="passenger" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
@@ -21,7 +54,7 @@
 
 </template>
 <script>
-import {defineComponent, ref} from 'vue';
+import {defineComponent, onMounted, ref} from 'vue';
 import {notification} from "ant-design-vue";
 import axios from "axios";
 
@@ -29,8 +62,16 @@ export default defineComponent({
 
   name: "passenger-view",
   setup() {
-
+    let loading = ref(false);
+    const passengers = ref([]);
     const visible = ref(false);
+
+    // 分页的三个属性名是固定的
+    const pagination = ref({
+      total: 0,
+      current: 1,
+      pageSize: 10,
+    });
 
     let passenger = ref({
       id: undefined,
@@ -41,6 +82,32 @@ export default defineComponent({
       createTime: undefined,
       updateTime: undefined,
     });
+    const columns = [
+      {
+        title: '会员id',
+        dataIndex: 'memberId',
+        key: 'memberId',
+      },
+      {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '身份证',
+        dataIndex: 'idCard',
+        key: 'idCard',
+      },
+      {
+        title: '旅客类型',
+        dataIndex: 'type',
+        key: 'type',
+      },
+      {
+        title: '操作',
+        dataIndex: 'operation'
+      }
+    ];
 
     // 分页的三个属性名是固定的
     // 点击就打开模态框
@@ -48,27 +115,101 @@ export default defineComponent({
       passenger.value = {};
       visible.value = true;
     };
-    const handleOk = () => {
-      axios.post("/member/passenger/save", passenger.value).then((response) => {
-        let data = response.data;
+
+    const onEdit = (record) => {
+      passenger.value = window.Tool.copy(record);
+      visible.value = true;
+    };
+    // 根据数据库的主键删除
+    const onDelete = (record) => {
+      axios.delete("/member/passenger/delete/" + record.id).then((response) => {
+        const data = response.data;
         if (data.success) {
-          notification.success({description: "保存成功！"});
-          visible.value = false;
-          // handleQuery({
-          //   page: pagination.value.current,
-          //   size: pagination.value.pageSize
-          // });
+          notification.success({description: "删除成功！"});
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize,
+          });
         } else {
           notification.error({description: data.message});
         }
       });
     };
 
+    // 新增乘客
+    const handleOk = () => {
+      axios.post("/member/passenger/save", passenger.value).then((response) => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({description: "保存成功！"});
+          visible.value = false;
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize
+          });
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+
+    const handleTableChange = (pagination) => {
+      // console.log("看看自带的分页参数都有啥：" + pagination);
+      handleQuery({
+        page: pagination.current,
+        size: pagination.pageSize
+      });
+    };
+
+    const handleQuery = (param) => {
+      if (!param) {
+        param = {
+          page: 1,
+          size: pagination.value.pageSize
+        };
+      }
+      loading.value = true;
+      axios.get("/member/passenger/query-list", {
+        params: {
+          page: param.page,
+          size: param.size
+        }
+      }).then((response) => {
+        // 函数的返回值，使用
+        loading.value = false;
+        let data = response.data;
+        if (data.success) {
+          passengers.value = data.content.list;
+          // 设置分页控件的值
+          pagination.value.current = param.page;
+          pagination.value.total = data.content.total;
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+    //
+    onMounted(() => {
+      handleQuery({
+        page: 1,
+        size: pagination.value.pageSize
+      });
+    });
     return {
-      onAdd,
-      handleOk,
+      // PASSENGER_TYPE_ARRAY,
+      passengers,
       passenger,
-      visible
+      visible,
+      pagination,
+      columns,
+      loading,
+
+      onEdit,
+      onAdd,
+      onDelete,
+      handleOk,
+      handleQuery,
+      handleTableChange
     };
   },
 });

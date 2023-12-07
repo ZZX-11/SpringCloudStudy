8b32433621969161330257d17d6befbcb12016c9
zzx <1102706742@QQ.com>
2023-12-07 14:56:24 +0800

    5.2 集成Quartz最简单的用法

--- batch/src/main/java/com/example/train/batch/job/SpringBootTestJob.java
+++ batch/src/main/java/com/example/train/batch/job/SpringBootTestJob.java
@@ -1,21 +1,21 @@
-package com.example.train.batch.job;// package com.jiawa.train.batch.job;
+//package com.example.train.batch.job;
 //
- import org.springframework.scheduling.annotation.EnableScheduling;
- import org.springframework.scheduling.annotation.Scheduled;
- import org.springframework.stereotype.Component;
-
- /**
-  * 适合单体应用，不适合集群
-  * 没法实时更改定时任务状态和策略
-  */
- @Component
- @EnableScheduling
- public class SpringBootTestJob {
-
-//     每5s执行一次
-     @Scheduled(cron = "0/5 * * * * ?")
-     private void test() {
-         // 增加分布式锁，解决集群问题
-         System.out.println("SpringBootTestJob TEST");
-     }
- }
+// import org.springframework.scheduling.annotation.EnableScheduling;
+// import org.springframework.scheduling.annotation.Scheduled;
+// import org.springframework.stereotype.Component;
+//
+// /**
+//  * 适合单体应用，不适合集群
+//  * 没法实时更改定时任务状态和策略
+//  */
+// @Component
+// @EnableScheduling
+// public class SpringBootTestJob {
+//
+////     每5s执行一次
+//     @Scheduled(cron = "0/5 * * * * ?")
+//     private void test() {
+//         // 增加分布式锁，解决集群问题
+//         System.out.println("SpringBootTestJob TEST");
+//     }
+// }

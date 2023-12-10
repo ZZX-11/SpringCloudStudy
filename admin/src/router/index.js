627bfa633daf2af04929a35a171cd41924ca3a66
zzx <1102706742@QQ.com>
2023-12-10 21:04:12 +0800

    6.3 生成每日车站页面与代码

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -31,10 +31,29 @@ const routes = [{
         }]
     }, {
         path: 'business/',
-        children: [{
+        children: [
+        //     {
+        //     path: 'confirm-order',
+        //     component: () => import('../views/main/business/confirm-order.vue'),
+        // },
+            {
             path: 'daily-train',
             component: () => import('../views/main/business/daily-train.vue'),
-        }]
+        }, {
+            path: 'daily-train-station',
+            component: () => import('../views/main/business/daily-train-station.vue'),
+        }
+        // , {
+        //     path: 'daily-train-carriage',
+        //     component: () => import('../views/main/business/daily-train-carriage.vue'),
+        // }, {
+        //     path: 'daily-train-seat',
+        //     component: () => import('../views/main/business/daily-train-seat.vue'),
+        // }, {
+        //     path: 'daily-train-ticket',
+        //     component: () => import('../views/main/business/daily-train-ticket.vue'),
+        // }
+        ]
     }, {
         path: 'batch/',
         children: [{

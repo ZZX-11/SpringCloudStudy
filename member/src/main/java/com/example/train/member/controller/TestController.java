75496efcb54e9d81def130a5dc1a8d0c883e574d
zzx <1102706742@QQ.com>
2023-12-16 15:53:00 +0800

    7.1 集成Nacos1

--- member/src/main/java/com/example/train/member/controller/TestController.java
+++ member/src/main/java/com/example/train/member/controller/TestController.java
@@ -1,12 +1,20 @@
 package com.example.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
+// 这个域可以被实时的刷新
 public class TestController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!"+testNacos;
     }
+
+    @Value("${test.nacos}")
+    private String testNacos;
+
 }
